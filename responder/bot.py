28fe74e9a0a6cf4f060312343532fb9b25cfba96
Yudhistira Erlandinata <yerlandinata@gmail.com>
2017-05-04 01:33:50 +0700

    add print statement

--- responder/bot.py
+++ responder/bot.py
@@ -31,6 +31,7 @@ def base(request):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    print(event)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text="Hello world!"))
